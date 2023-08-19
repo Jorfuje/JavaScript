dab64da210f2a608a96119e81c9ed292484cbffc
Jorfuje <arbjorfuje@gmail.com>
2023-08-19 14:17:59 -0600

    Aplicación Clima terminada

---
+++ CLIMA/script.js
@@ -0,0 +1,46 @@
+let api_key = "b00e97f09811a0fd22be35df70ae44b0";
+let difKelvin = 273.15;
+let urlBase = 'http://api.openweathermap.org/data/2.5/weather';
+
+document.getElementById('botonBusqueda').addEventListener('click', () => {
+    const ciudad = document.getElementById('ciudadEntrada').value;
+    if(ciudad){
+        fetchDatosClima(ciudad);
+    }
+})
+
+function fetchDatosClima(ciudad){
+    fetch(`${urlBase}?q=${ciudad}&APPID=${api_key}`)
+    .then(data => data.json())
+    .then(data => mostrarDatosClima(data))
+}
+
+function mostrarDatosClima(data){
+    const divDatosClima = document.getElementById('datosClima');
+    divDatosClima.innerHTML = '';
+
+    const ciudadNombre = data.name;
+    const paisNombre = data.sys.country;
+    const temperatura = data.main.temp;
+    const humedad = data.main.humidity;
+    const descripcion = data.weather[0].description
+
+    const ciudadTitulo = document.createElement('h2');
+    ciudadTitulo.textContent = `${ciudadNombre}, ${paisNombre}`;
+
+    const temperaturaInfo = document.createElement('p');
+    temperaturaInfo.textContent = `La temperatura es: ${Math.floor(temperatura-difKelvin)}°C`;
+    
+    const humedadInfo = document.createElement('p');
+    humedadInfo.textContent = `La humedad es: ${humedad}%`;
+
+    const descripcionInfo = document.createElement('p');
+    descripcionInfo.textContent = `La descripción meteorológica es: ${descripcion}`;
+
+    divDatosClima.appendChild(ciudadTitulo);
+    divDatosClima.appendChild(temperaturaInfo);
+    divDatosClima.appendChild(humedadInfo);
+    divDatosClima.appendChild(descripcionInfo);
+}
+
+
